1938687355ac66fd41d5e2bdd95c7ba0c362d3ee
Daehyeon Kim <104838522@student.swin.edu.au>
2025-04-24 12:43:01 +1000

    add lab007part2 zip and processbooking.php

---
+++ Lab07-Part2/processbooking.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Booking Confirmation</title>
+</head>
+<body>
+    <h1>Rohirrim Tour Booking Confirmation</h1>
+</body>
+</html>
